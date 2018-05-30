ae29d11b1fcad8fa7ea6f2838f94fe54dcb32246
谢鹏 <xiepeng@xiepengdeMBP.office.mimikko.cn>
2018-05-30 13:32:58 +0800

    注释掉redis test commands

--- scripts/test_init.sh
+++ scripts/test_init.sh
@@ -13,11 +13,11 @@ docker run --rm --network redis --volumes-from scripts \
 	registry.docker-cn.com/library/redis:4.0.9-alpine sh /scripts/check_scaling.sh 2 2
 
 
-docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -h 10.0.0.12 -p 6379 -c 50 -n 10000
-
-docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -t set -r 100000 -n 1000000
-docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -h 10.0.0.12 -r 1000000 -n 2000000 -t get,set,lpush,lpop -P 16 -q > ~/redis-0.log
-
-docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -h 10.0.0.12 -r 1000000 -n 2000000 -t get,set,lpush,lpop -P 16 -q > ~/redis-1.log
-
-docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -h 10.0.0.12 -r 1000000 -n 2000000 -t get,set,lpush,lpop -P 16 -q > ~/redis-2.log
+# docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -h 10.0.0.12 -p 6379 -c 50 -n 10000
+#
+# docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -t set -r 100000 -n 1000000
+# docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -h 10.0.0.12 -r 1000000 -n 2000000 -t get,set,lpush,lpop -P 16 -q > ~/redis-0.log
+#
+# docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -h 10.0.0.12 -r 1000000 -n 2000000 -t get,set,lpush,lpop -P 16 -q > ~/redis-1.log
+#
+# docker run --rm --network redis registry.docker-cn.com/library/redis:4.0.9-alpine redis-benchmark -h 10.0.0.12 -r 1000000 -n 2000000 -t get,set,lpush,lpop -P 16 -q > ~/redis-2.log
