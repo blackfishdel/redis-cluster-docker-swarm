588400760a15d3382024a8a9887f225bcc577ac1
谢鹏 <xiepeng@xiepengdeMBP.office.mimikko.cn>
2018-06-20 22:45:33 +0800

    test

--- test.sh
+++ test.sh
@@ -6,4 +6,7 @@ docker run --rm --network redis registry.int.mimikko.cn/redis-utils:latest \
   	value "num-slaves"
 
 docker run --rm --network redis registry.int.mimikko.cn/redis-utils:latest \
-	redis-sentinel1 redismaster reset "num-slaves" "$((NUM_OF_REDIS - 1))"  
+	redis-sentinel1 redismaster reset "num-slaves" "$((NUM_OF_REDIS - 1))"
+
+
+redis-cli -h 139.219.142.114 -p 26380 sentinel master redismaster
