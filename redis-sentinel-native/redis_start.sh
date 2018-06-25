9cd5847e93b7062c3df996b6a8cc3c5ef2521ec1
xiepeng <se_xiepeng@hetaozs.com>
2018-06-25 21:35:05 +0800

    自定义redis启动 fix1

--- redis-sentinel-native/redis_start.sh
+++ redis-sentinel-native/redis_start.sh
@@ -15,21 +15,21 @@ sed -i "s/{{ REDIS_PORT }}/$REDIS_PORT1/g" ./redis1.conf
 sed -i "s/{{ REDIS_PORT }}/$REDIS_PORT2/g" ./redis2.conf
 sed -i "s/{{ REDIS_PORT }}/$REDIS_PORT3/g" ./redis3.conf
 
-redis-server /redis/redis1.conf
-redis-server /redis/redis2.conf --slaveof $REDIS_MASTER_IP $REDIS_MASTER_PORT
-redis-server /redis/redis3.conf --slaveof $REDIS_MASTER_IP $REDIS_MASTER_PORT
+redis-server ./redis1.conf
+redis-server ./redis2.conf --slaveof $REDIS_MASTER_IP $REDIS_MASTER_PORT
+redis-server ./redis3.conf --slaveof $REDIS_MASTER_IP $REDIS_MASTER_PORT
 
 
 
 for i in {1..3} ; do
-  sed -i "s/{{ SENTINEL_PORT }}/2638$i/g" /redis/sentinel$i.conf
-  sed -i "s/{{ REDIS_MASTER_NAME }}/$REDIS_MASTER_NAME/g" /redis/sentinel$i.conf
-  sed -i "s/{{ REDIS_MASTER_IP }}/$REDIS_MASTER_IP/g" /redis/sentinel$i.conf
-  sed -i "s/{{ REDIS_MASTER_PORT }}/$REDIS_MASTER_PORT/g" /redis/sentine$i.conf
+  sed -i "s/{{ SENTINEL_PORT }}/2638$i/g" ./sentinel$i.conf
+  sed -i "s/{{ REDIS_MASTER_NAME }}/$REDIS_MASTER_NAME/g" ./sentinel$i.conf
+  sed -i "s/{{ REDIS_MASTER_IP }}/$REDIS_MASTER_IP/g" ./sentinel$i.conf
+  sed -i "s/{{ REDIS_MASTER_PORT }}/$REDIS_MASTER_PORT/g" ./sentine$i.conf
 
-  sed -i "s/{{ SENTINEL_QUORUM }}/$SENTINEL_QUORUM/g" /redis/sentine$i.conf
-  sed -i "s/{{ SENTINEL_DOWN_AFTER }}/$SENTINEL_DOWN_AFTER/g" /redis/sentinel$i.conf
-  sed -i "s/{{ SENTINEL_FAILOVER }}/$SENTINEL_FAILOVER/g" /redis/sentinel$i.conf
+  sed -i "s/{{ SENTINEL_QUORUM }}/$SENTINEL_QUORUM/g" ./sentine$i.conf
+  sed -i "s/{{ SENTINEL_DOWN_AFTER }}/$SENTINEL_DOWN_AFTER/g" ./sentinel$i.conf
+  sed -i "s/{{ SENTINEL_FAILOVER }}/$SENTINEL_FAILOVER/g" ./sentinel$i.conf
 
-  redis-server /redis/sentinel$i.conf --sentinel
+  redis-server ./sentinel$i.conf --sentinel
 done
