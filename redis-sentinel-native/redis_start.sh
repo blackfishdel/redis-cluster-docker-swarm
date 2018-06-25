15bf10baf4797e6e4da10324666f9f13e81cd250
xiepeng <se_xiepeng@hetaozs.com>
2018-06-25 21:48:20 +0800

    自定义redis启动 fix3

--- redis-sentinel-native/redis_start.sh
+++ redis-sentinel-native/redis_start.sh
@@ -15,9 +15,9 @@ sed -i "s/{{ REDIS_PORT }}/$REDIS_PORT1/g" ./redis1.conf
 sed -i "s/{{ REDIS_PORT }}/$REDIS_PORT2/g" ./redis2.conf
 sed -i "s/{{ REDIS_PORT }}/$REDIS_PORT3/g" ./redis3.conf
 
-redis-server ./redis1.conf
-redis-server ./redis2.conf --slaveof $REDIS_MASTER_IP $REDIS_MASTER_PORT
-redis-server ./redis3.conf --slaveof $REDIS_MASTER_IP $REDIS_MASTER_PORT
+/root/redis-4.0.10/src/redis-server ./redis1.conf
+/root/redis-4.0.10/src/redis-server ./redis2.conf --slaveof $REDIS_MASTER_IP $REDIS_MASTER_PORT
+/root/redis-4.0.10/src/redis-server ./redis3.conf --slaveof $REDIS_MASTER_IP $REDIS_MASTER_PORT
 
 
 
@@ -31,5 +31,5 @@ for i in {1..3} ; do
   sed -i "s/{{ SENTINEL_DOWN_AFTER }}/$SENTINEL_DOWN_AFTER/g" ./sentinel$i.conf
   sed -i "s/{{ SENTINEL_FAILOVER }}/$SENTINEL_FAILOVER/g" ./sentinel$i.conf
 
-  redis-server ./sentinel$i.conf --sentinel
+  /root/redis-4.0.10/src/redis-server ./sentinel$i.conf --sentinel
 done
